8b72db7fe1c479ba6d791fcaca3a27f56a615cc8
BenyamWorku <benworku@gmail.com>
2023-05-07 13:06:18 +0100

    creates admin, social worker, tutor pages

---
+++ srmnext/src/pages/socialworker.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+function SocialworkerPage() {
+  return <h1>This is the social worker page.</h1>;
+}
+
+export default SocialworkerPage;
---
+++ srmnext/src/pages/tutor.tsx
@@ -0,0 +1,7 @@
+/** @format */
+
+function TutorPage() {
+  return <h1>This is the Tutor page.</h1>;
+}
+
+export default TutorPage;
